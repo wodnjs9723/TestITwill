e579e9997d08d2f79d823d5fc945cc48ce3f4ed2
wodnjs9723 <33622902+wodnjs9723@users.noreply.github.com>
2019-02-26 12:35:57 +0900

    Update Test1.java

--- JavaGitTest/src/Test1.java
+++ JavaGitTest/src/Test1.java
@@ -3,7 +3,8 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		
-			System.out.println("안녕하세요.");
+		System.out.println("안녕하세요.");
+		System.out.println("GitHub에서 작성한 문장입니다.");
 	}
 
 }
